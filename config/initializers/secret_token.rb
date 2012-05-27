ffd8ccbbbc15111b0ae8d9c49d807460207f1666
Pomidorus <andrewaka39@gmail.com>
2012-05-27 20:22:19 +0300

    dd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomi02::Application.config.secret_token = '77d866bd8774ef9a908d0a96d1d819ce914739b1e57371d79bfb7a849154871f7a7edfa8aaa65f96c7ceab25c1bb7ee313f4e183ff1b977c9187290ef86d267d'
